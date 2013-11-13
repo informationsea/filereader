4af6cc96befa145a678cff1fbcc0f8ab459f4e89
Y.Okamura <okamura@informationsea.info>
2013-11-13 13:32:26 +0900

    if file name ends with .gz, GzipFileReader will be selected

--- src/filereader.cpp
+++ src/filereader.cpp
@@ -1,5 +1,7 @@
 #include "filereader.hpp"
 
+#include <string.h>
+
 FileReader *filereader_get_suitable_reader(int fd)
 {
 #ifndef _WIN32
@@ -19,6 +21,16 @@ FileReader *filereader_get_suitable_reader(int fd)
 
 FileReader *filereader_get_suitable_reader(const char* path)
 {
+    size_t pathlen = strlen(path);
+    fprintf(stderr, "%s %s\n", path + pathlen - 3, path);
+    if (strncmp(path+pathlen-3, ".gz", 3) == 0) {
+        fprintf(stderr, "Gzip\n");
+        GzipFileReader *gfr = new GzipFileReader();
+        if (gfr->open_path(path))
+            return gfr;
+        delete gfr;
+    }
+
 #ifndef _WIN32
     MmapReader *mmapreader = new MmapReader();
     if (mmapreader->open_path(path))
